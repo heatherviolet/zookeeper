2a3fefc311a21d5e61d9144a7b898f76eaa2eb67
Heather Violet <heather@heathers-air.lan>
2022-01-17 19:16:15 -0800

    added functions and .get

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const e = require('express');
 
 const PORT = process.env.PORT || 3001;
 
@@ -44,6 +45,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -51,6 +57,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
   });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
